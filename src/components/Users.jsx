1f66104acd9e91b05d5b450e65086b0a4aa4a840
gerald-M14 <geraldhymessu7@gmail.com>
2025-02-28 14:06:43 -0500

    Responsive

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -22,54 +22,51 @@ const Users = () => {
     return (
         
         <div className="container mx-auto p-4">
-                    
 
-                    {/* fondo */}
-                    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-                    <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
-                    </div>
+      {/* fondo */}
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
+      </div>
 
-
-        <div className="grid gap-4">
+      <div className="grid gap-4">
         
-            <Navbar />
+        <Navbar />
         
-            <h1 className="text-5xl font-bold mb-4">Users</h1>
-            <table className="table-auto w-full border-collapse border border-gray-300">
-                <thead>
-                    <tr className="text-white bg-gray-800">
-                        <th className="border border-gray-300 px-4 py-2">ID</th>
-                        <th className="border border-gray-300 px-4 py-2">Name</th>
-                        <th className="border border-gray-300 px-4 py-2">Username</th>
-                        <th className="border border-gray-300 px-4 py-2">Email</th>
-                        <th className="border border-gray-300 px-4 py-2">Actions</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        users.map((user) => (
-                            <tr key={user.id} className="even:bg-gray-100">
-                                <td className="border border-gray-300 px-4 py-2">{user.id}</td>
-                                <td className="border border-gray-300 px-4 py-2">{user.name}</td>
-                                <td className="border border-gray-300 px-4 py-2">{user.username}</td>
-                                <td className="border border-gray-300 px-4 py-2">{user.email}</td>
-                                <td className="border border-gray-300 px-4 py-2">
-                                <Link 
-                                    to={`/posts/${user.id}`} 
-                                    className="text-white px-4 py-1 rounded hover:bg-[#bdf8ff] hover:border-gray-800 hover:text-black focus:outline-none focus:ring-4 focus:ring-blue-300 transition duration-300 bg-gray-500"
-                                    >
-                                    Posts
-                                    </Link>
-
-                                </td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">Users</h1>
+        
+        <div className="overflow-x-auto">
+          <table className="table-auto w-full border-collapse border border-gray-300">
+            <thead>
+              <tr className="text-white bg-gray-800">
+                <th className="border border-gray-300 px-4 py-2">ID</th>
+                <th className="border border-gray-300 px-4 py-2">Name</th>
+                <th className="border border-gray-300 px-4 py-2">Username</th>
+                <th className="border border-gray-300 px-4 py-2">Email</th>
+                <th className="border border-gray-300 px-4 py-2">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id} className="even:bg-gray-100">
+                  <td className="border border-gray-300 px-4 py-2">{user.id}</td>
+                  <td className="border border-gray-300 px-4 py-2">{user.name}</td>
+                  <td className="border border-gray-300 px-4 py-2">{user.username}</td>
+                  <td className="border border-gray-300 px-4 py-2">{user.email}</td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    <Link 
+                      to={`/posts/${user.id}`} 
+                      className="text-white px-4 py-1 rounded hover:bg-[#bdf8ff] hover:border-gray-800 hover:text-black focus:outline-none focus:ring-4 focus:ring-blue-300 transition duration-300 bg-gray-500"
+                    >
+                      Posts
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-</div>
+      </div>
+    </div>
 
     )
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,23 +6,25 @@ import { Link } from 'react-router-dom';
 export const Welcome = () => {
     return (
     
-<div className="min-h-screen flex justify-center items-center">
+        <div className="min-h-screen flex flex-col justify-center items-center sm:flex-row">
 
-    {/* fondo */}
-    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
-    </div>
+            {/* fondo */}
+            <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+            <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
+            </div>
 
-    <div className="container flex justify-center items-center flex-col w-2xl"> 
-        <div>
-            <h1 className="text-7xl font-medium">Hello, welcome to the user and publication portal.</h1>
+            <div className="container flex flex-col justify-center items-center w-full px-4 sm:px-0">
+            <div className="text-center">
+          <h1 className="text-3xl sm:text-7xl font-medium">Hello, welcome to the user and publication portal.</h1>
         </div>
-        <div className='mt-5 w-lg'>
-            <p className="text-2xl text-gray-600">On this page you can find users and see the posts they have made.</p>
+            <div className='mt-5 sm:w-1/2'>
+                <p className="text-lg sm:text-2xl text-gray-600">On this page you can find users and see the posts they have made.</p>
+            </div>
+            <Link to="/Users" className="mt-10 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 sm:me-2 sm:mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                View users
+            </Link>
+            </div>
         </div>
-        <Link to="/Users" className="mt-10 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">View users</Link>
-    </div>
-</div>
 
 
     )
